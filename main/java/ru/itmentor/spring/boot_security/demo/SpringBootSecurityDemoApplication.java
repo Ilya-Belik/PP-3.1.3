9bb8634f4386dea92044e75bee34d7d5b90ce290
Ilya Belik <141572840+Ateglif@users.noreply.github.com>
2023-12-17 16:55:18 +0500

    Add files via upload

---
+++ main/java/ru/itmentor/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -0,0 +1,18 @@
+package ru.itmentor.spring.boot_security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringBootSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+	}
+
+}
