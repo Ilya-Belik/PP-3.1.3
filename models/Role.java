6044a1288393e93964bb6fd977c83033902da47f
Ilya Belik <141572840+Ateglif@users.noreply.github.com>
2024-01-16 14:12:22 +0500

    Update Role.java

--- models/Role.java
+++ models/Role.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@JsonIgnoreProperties({"person"})
 @Entity
 @Table(name="roles")
 public class Role implements GrantedAuthority {
@@ -15,10 +14,6 @@ public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @ManyToOne
-    @JoinColumn(name = "person_id")
-    private Person person;
 
     @Column(name = "name_role")
     private String name;
@@ -45,14 +40,6 @@ public class Role implements GrantedAuthority {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
     }
 
     @Override
