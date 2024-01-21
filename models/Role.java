7ce8e33bac6684d1ababd4d99ac54cf4d758c02b
Ilya Belik <141572840+Ateglif@users.noreply.github.com>
2024-01-21 14:47:51 +0500

    Update Role.java

--- models/Role.java
+++ models/Role.java
@@ -1,5 +1,6 @@
 package ru.itmentor.spring.boot_security.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
@@ -17,6 +18,7 @@ public class Role implements GrantedAuthority {
     private String name;
 
     @ManyToMany(mappedBy = "roles")
+    @JsonIgnoreProperties("roles")
     private Set<Person> persons;
 
     public Role() {
